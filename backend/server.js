7c43a6b0f203fc4483b992c97368b39c278a603a
Mano-Bharathii <manobharathi189@gmail.com>
2025-12-26 13:28:00 +0530

    Changed Server port and added frontend url env variable in the server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'https://localhost:5173',
-  'https://127.0.0.1:5173'
+  'https://127.0.0.1:5173',
+  process.env.FRONTEND_URL,
 ];
 
 app.use(cookieParser());
